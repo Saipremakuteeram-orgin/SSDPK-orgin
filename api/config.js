19b137c21a3f298a4403c706d6e223ca37495187
Sathya Sai Trust <sk143sathya@gmail.com>
2026-05-26 11:10:13 +0530

    Configure vercel.json routing and CommonJS export for api/config.js

--- api/config.js
+++ api/config.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
   // CORS configuration
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
@@ -14,4 +14,4 @@ export default function handler(req, res) {
     SUPABASE_URL: process.env.SUPABASE_URL || "",
     SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY || ""
   });
-}
+};
